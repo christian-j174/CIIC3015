2cb30924146a7caeb00f66a248c6b3196e0c64e9
3015b5 <christian.rodriguez95@upr.edu>
2022-02-09 18:11:01 -0400

    last review

--- LAB#3/main.py
+++ LAB#3/main.py
@@ -1,1 +1,14 @@
-print(2)
+
+#Problem #1
+# a = "The cake was"
+# b = 13.56
+# c = "inches tall!"
+#
+#
+# print(f'{a} {b} {c}')
+
+
+#PROBLEM #2
+# print("Who's making all that noise?")
+# print("I'd like to order Rosie's special, the \"Grand Omelette\".")
+
