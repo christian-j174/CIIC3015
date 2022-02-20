e9149a1bf28ee2f2d662c76848e47e99187c2f25
chris <christian.rodriguez95@upr.edu>
2022-02-19 20:59:14 -0400

    completed PROJECT#1

--- PROJECT1/main.py
+++ PROJECT1/main.py
@@ -1,10 +1,15 @@
-#                       Bank Account Simulator
-#	BANK ACCOUNT SIMULATOR
+#by Christian J. Rodriguez and Michelle Bou
 
+# BANK ACCOUNT SIMULATOR
 account_name = str( input( "Name for your account: " ) )
-balance = float( input( "What is your starting balance?: $" ) )
+try:
+    balance = float( input( "What is your starting balance?: $" ) )
+except:
+    print('FOR YOUR STARTING BALANCE, YOU MUST ENTER ONLY NUMBERS')
+    balance = float( input( "What is your starting balance?: $" ) )
 starting_balance = balance  # this varible is use to calculate the amount of deposits doned
 run = True
+
 options = """1. Deposit some funds
 2. Withdraw some funds
 3. View balance 
@@ -22,14 +27,24 @@ penalty_received = 0
 successful_withdrawn = 0
 unsuccessful_withdrawn = 0
 
-while run:
+while run:      #MAIN LOOP
     print( f'\tACCOUNT NAME: {account_name}\n' )
     print( options )
-    user_choice = int( input( "/>" ) )
+    try:        #MANAGE ILEGAL INPUTS
+        user_choice = int( input( "/>" ) )
+    except:
+        print('ENTER A INTEGRER NUMBER')
+        print(options)
+        user_choice = int( input( "/>" ) )
+
     if user_choice == 1:
-        user_input = float( input( 'How much you want to deposit ?: $' ) )
-        if user_input > balance:  # Sum the balance and 2% reward
-            balance += (user_input * 1.02)  # add the balance and the reward
+        try:
+            user_input = float( input( 'How much you want to deposit ?: $' ) )
+        except:
+            print('ENTER A NUMBER')
+            user_input = float( input( 'How much you want to deposit ?: $' ) )
+        if user_input > balance:        # Sum the balance and 2% reward
+            balance += (user_input * 1.02)      # add the balance and the reward
             balance = round(balance, 2)
             bonus_received += (user_input * 1.02) - user_input
             bonus_received = round(bonus_received, 2)
@@ -37,31 +52,42 @@ while run:
             log_reward = +1
             amount_of_deposits += user_input
             print( f'Your new balance is ${balance}' )
-        else:
+        else:       #THE DEPOSIT IS LESS THAN THE CURRENT BALANCE
             balance += user_input
             log_deposit += 1
             amount_of_deposits += user_input
             print( f'Your new balance is ${balance}' )
 
     elif user_choice == 2:
-        user_input = float( input( "How much you want to withdraw?: $" ) )
+        try:
+            user_input = float( input( "How much you want to withdraw?: $" ) )
+        except:
+            print('ENTER A NUMBER')
+            user_input = float( input( "How much you want to withdraw?: $" ) )
+
         if user_input > balance:  # conditional to charge a penalty fee
             balance -= 5
             penalty_received += 5
             unsuccessful_withdrawn += user_input
             log_penalty += 1
+            print("you do not have the minimum balance to be able to withdraw the money")
             print( f'Your new balance is ${balance}' )
-        else:
+        else:       #IF THIS RUNS, IT MEANS YOU HAVE THE SUFFICIENT BALANCE
             balance -= user_input
             successful_withdrawn += user_input
             log_withdrawn += 1
             print( f'Your new balance is ${balance}' )
-    elif user_choice == 3:
-        print( f'Your current balance is: ${balance}' )
-    elif user_choice == 4:
+
+    elif user_choice == 3:      #PRINT THE CURRENT BALANCE
+        print( f'\tYour current balance is: ${balance}' )
+    elif user_choice == 4:      #RESET ALL VARIABLES AND CREATE NEW ACCOUNT
         print( 'ALERT YOU WILL CREATE A NEW ACCOUNT AND THE PREVIUS DATA WILL BE RESETED\n' )
         account_name = str( input( "Name for your account: " ) )
-        balance = float( input( "What is your starting balance?: $" ) )
+        try:
+            balance = float( input( "What is your starting balance?: $" ) )
+        except:
+            print( 'FOR YOUR STARTING BALANCE, YOU MUST ENTER ONLY NUMBERS' )
+            balance = float( input( "What is your starting balance?: $" ) )
         log_reward = 0
         log_penalty = 0
         log_deposit = 0
@@ -72,11 +98,11 @@ while run:
         successful_withdrawn = 0
         unsuccessful_withdrawn = 0
 
-    elif user_choice == 5:
+    elif user_choice == 5:      #PRINT THE VALUES OF ALL VARIABLES
         print( '\tFINAL BALANCE' )
         print(f'Your final balance is: ${balance}')
         print( f'The total of times attempted to deposit: {log_deposit}' )
-        print( f'The total amount of fund deposited: {amount_of_deposits}' )
+        print( f'The total amount of fund deposited: ${amount_of_deposits}' )
         print( f'The total of times a bonus was received: {log_reward}' )
         print( f'The total of bonus received: ${bonus_received}' )
         print( f'The total of times attempted to withdrawn funds successfully: {log_withdrawn}' )
