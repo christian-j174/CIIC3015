753e9013ce7d2b08810db23379567cceaec27311
chris <christian.rodriguez95@upr.edu>
2022-04-03 18:33:22 -0400

    completed lab

--- PROJECT #2/qqq.py
+++ PROJECT #2/qqq.py
@@ -1,26 +1,28 @@
-cook = True
-units = ['cups', 'tablespoons', '', 'cups', 'teaspoons', 'teaspoons', 'slices', '']
-ingredients = ['flour', 'sugar', 'eggs', 'milk', 'cinnamon', 'baking powder', 'bread', 'fruits']
-banana_pancake_recipe = [1, 2, 1, 1, 3, 2, 0, 2]
-peach_crepe_recipe = [1, 0, 1, 1, 2, 0, 0, 3]
-apple_pie_recipe = [2, 4, 2, 0.5, 1, 1, 0, 5]
-french_toast_recipe = [0.5, 3, 3, 0.5, 2, 0, 8, 0]
-scrambled_eggs = [0, 0, 4, 0.5, 0, 0, 2, 1.5]
-menu = [banana_pancake_recipe, peach_crepe_recipe, apple_pie_recipe, french_toast_recipe, scrambled_eggs]
-menu_list = ["banana pancake", "peach crepe", "apple pie", "french toast", "scrambled eggs with toast and fruits"]
+def translator():
+    alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+             'V', 'W', 'X', 'Y', 'Z']
 
-def show_recipe(list):
-    if list:
-        print( 'Ingredients needed for selected recipe:' )
-        yy = -1
-        if yy < 5:
-            yy += 1
-        for i in range( 0, 8 ):
+    n = [2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9]
 
-            if i == 7 or i == 2:
-                print( f"{list[i]} {ingredients[i]}" )
-            else:
-                print( f"{list[i]} {units[i]} of {ingredients[i]}" )
+    num = input( 'Enter phone number in format of XXX-XXX-XXXX: ' ).upper()
+
+    index = 0
+    x = ''
+
+    for index in range( len( num ) ):
+
+        if num[index].isalpha():
+
+            x += str( n[alpha.index( num[index] )] )
+
+        else:
+            continue
+            x += num[index]
+
+    ss = x[0:2] + '-' + x[5:7] + '-' + x[9:12]
+
+    return f"The number is {x}"
 
 
-show_recipe([3,4,5,6,7,8,9,10])
+print(translator())
+
